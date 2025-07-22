9e5a00d4a531f8b9058953eb50b800a8c625164a
Kazım İncebay <kazimincebay@gmail.com>
2025-07-22 19:35:50 +0300

    I created about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import bannerImage from '../assets/banneryeni.jpg'
+import '../styles/About.css';
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+      <div className='aboutTop' style={{backgroundImage:`url(${bannerImage})`}}></div>
+      <div className='aboutBottom'>
+<h1>Hakkımızda</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut autem culpa quis animi! Quidem blanditiis officia nesciunt dolorem ullam iste cumque minus architecto ratione magnam nisi voluptas, molestias nemo assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur veritatis velit nulla, at harum magni adipisci recusandae deleniti maiores, esse tempora, illum sed illo dolorem. Ipsam in nihil qui nemo? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, non, necessitatibus libero in, aperiam quisquam sint modi exercitationem deleniti reiciendis temporibus minima dolor reprehenderit id cumque? Commodi dolore corrupti deserunt.</p>
+      </div>
+    </div>
   )
 }

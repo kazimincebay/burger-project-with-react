d62d729ff914aaa699023f8671201b3f0c3ba20e
Kazım İncebay <kazimincebay@gmail.com>
2025-07-22 17:23:51 +0300

    I cleaned somethings in this project and I created Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Menu } from "./components/Menu";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      Kazım
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/menu" element={<Menu />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }

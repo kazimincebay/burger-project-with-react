623907f6605a2a115bf0a2e0007244a758a159a0
Kazım İncebay <kazimincebay@gmail.com>
2025-07-22 19:44:04 +0300

    I created contact component and I finished this project

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import bannerImage from '../assets/banneryeni.jpg'
-import '../styles/About.css';
+import React from "react";
+import bannerImage from "../assets/banneryeni.jpg";
+import "../styles/About.css";
 
 export const About = () => {
   return (
-    <div className='about'>
-      <div className='aboutTop' style={{backgroundImage:`url(${bannerImage})`}}></div>
-      <div className='aboutBottom'>
-<h1>Hakkımızda</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut autem culpa quis animi! Quidem blanditiis officia nesciunt dolorem ullam iste cumque minus architecto ratione magnam nisi voluptas, molestias nemo assumenda. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur veritatis velit nulla, at harum magni adipisci recusandae deleniti maiores, esse tempora, illum sed illo dolorem. Ipsam in nihil qui nemo? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, non, necessitatibus libero in, aperiam quisquam sint modi exercitationem deleniti reiciendis temporibus minima dolor reprehenderit id cumque? Commodi dolore corrupti deserunt.</p>
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${bannerImage})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1>Hakkımızda</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut autem
+          culpa quis animi! Quidem blanditiis officia nesciunt dolorem ullam
+          iste cumque minus architecto ratione magnam nisi voluptas, molestias
+          nemo assumenda. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Consequuntur veritatis velit nulla, at harum magni adipisci
+          recusandae deleniti maiores, esse tempora, illum sed illo dolorem.
+          Ipsam in nihil qui nemo? Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Officia, non, necessitatibus libero in, aperiam
+          quisquam sint modi exercitationem deleniti reiciendis temporibus
+          minima dolor reprehenderit id cumque? Commodi dolore corrupti
+          deserunt.
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import bannerImage from '../assets/banner.png';
+import '../styles/Contact.css';
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className='leftSide' style={{backgroundImage:`url(${bannerImage})`}}>
+        
+      </div>
+      <div className='rightSide'><h1>Bizimle İletişime Geçin</h1>
+<form>
+  <label>Ad Soyad</label>
+  <input type="text" name="name" placeholder='Lütfen adınızı soyadınızı giriniz...' />
+
+  <label>E-mail</label>
+  <input type="email" name="email" placeholder='Lütfen emailinizi giriniz...' />
+
+  <label>Mesajınız</label>
+  <textarea rows="6" name="message" placeholder='Lütfen mesajınızı giriniz...' ></textarea>
+
+</form></div>
+
+    </div>
   )
 }
